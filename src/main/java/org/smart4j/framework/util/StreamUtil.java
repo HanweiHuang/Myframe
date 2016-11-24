0a84be64a8074963a0609fb0971dbf381c389379
harveyNorman <wei2215038@gmail.com>
2016-11-25 02:22:40 +1100

    add copystream function，write inputstream to outputstream

--- src/main/java/org/smart4j/framework/util/StreamUtil.java
+++ src/main/java/org/smart4j/framework/util/StreamUtil.java
@@ -1,8 +1,10 @@
 package org.smart4j.framework.util;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,5 +27,28 @@ public final class StreamUtil {
 			throw new RuntimeException();
 		}
 		return sb.toString();
+	}
+	
+	//把输入的inputstream 输出 提供了inputstram 和 outputstream了 就是write一下
+	public static void copyStream(InputStream inputStream, OutputStream outputStream){
+		try{
+			int length;
+			byte[] buffer = new byte[4*1024];
+			while((length = inputStream.read(buffer,0,buffer.length))!=-1){
+				outputStream.write(buffer,0,length);
+			}
+			
+		}catch(Exception e){
+			LOGGER.error("copy stream failure", e);
+		}finally{
+			try {
+				inputStream.close();
+				outputStream.close();
+			} catch (IOException e) {
+				LOGGER.error("close stream failure",e);
+				e.printStackTrace();
+			}
+			
+		}
 	}
 }
